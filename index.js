bc503c6e49e5d372bf4226a89ee3819698ca8899
Raju Ahammad <25717712+rajuahammadfanz@users.noreply.github.com>
2019-08-05 16:46:38 +0600

    update

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 const userName = document.querySelector(".username"),
-  action = document.querySelector(".action"),
-  card = document.querySelector(".card"),
-  avatar = document.querySelector(".avatar"),
-  bio = document.querySelector(".bio"),
-  repoList = document.querySelector(".repo"),
-  url = document.querySelector(".url"),
-  name = document.querySelector(".name");
+    action = document.querySelector(".action"),
+    card = document.querySelector(".card"),
+    avatar = document.querySelector(".avatar"),
+    bio = document.querySelector(".bio"),
+    repoList = document.querySelector(".repo"),
+    url = document.querySelector(".url"),
+    name = document.querySelector(".name"),
+    bodyBg = document.querySelector("body");
+
+
+bodyBg.style.backgroundImage = `url('https://source.unsplash.com/random/1920x1080')`;
 
 // API Info
 const BASE = `https://api.github.com/users/`;
 
 action.addEventListener("click", f => {
-  const userNameValue = userName.value;
-  if (userNameValue) {
-    fetch(BASE + userNameValue)
-      .then(response => response.json())
-      .then(data => {
-        avatar.src = data.avatar_url;
-        name.innerHTML = data.name;
-        bio.innerHTML = data.bio;
-        repoList.href = data.html_url + "?tab=repositories";
-        url.href = data.blog;
-      })
+    const userNameValue = userName.value;
+    if (userNameValue) {
+        fetch(BASE + userNameValue)
+            .then(response => response.json())
+            .then(data => {
+                avatar.src = data.avatar_url;
+                name.innerHTML = data.name;
+                bio.innerHTML = data.bio;
+                repoList.href = data.html_url + "?tab=repositories";
+                url.href = data.blog;
+            })
 
-      .catch(error => {
-        console.log(error);
-      });
-    setTimeout(f => {
-      card.style.display = "block";
-    }, 1000);
-  }
+            .catch(error => {
+                console.log(error);
+            });
+        setTimeout(f => {
+            card.style.display = "block";
+        }, 1000);
+    }
 });
